17a6ce0c39c69d1efa5fa925b1ce800e82e1ef88
SharpMars <70289680+SharpMars@users.noreply.github.com>
2024-03-27 20:31:28 +0100

    Add class selection

--- src/App.tsx
+++ src/App.tsx
@@ -4,44 +4,82 @@ import { ScheduleData, fetchEdupageData } from "./utils/edupage";
 import { useEffect, useState } from "react";
 import Lesson from "./components/lesson";
 import { Accent1 } from "./theme";
-import { Dropdown } from 'react-native-element-dropdown';
+import { Dropdown } from "react-native-element-dropdown";
 import DropdownComponent from "./components/dropdown";
 
 export default function App() {
   let [schedule, setSchedule] = useState<ScheduleData>();
+  let [selectedClass, setSelectedClass] = useState<number>(null);
+  let [lessonNames, setLessonNames] = useState<string[]>([]);
 
   useEffect(() => {
     fetchEdupageData().then((data) => setSchedule(data));
   }, []);
 
+  useEffect(() => {
+    if (schedule != undefined) {
+      const classId = schedule.classes.find((klasa) => klasa.id == selectedClass).id;
+      const groupId = schedule.groups.filter((grupa) => grupa.classId == classId);
+      setLessonNames(
+        schedule.lessons
+          .filter((lesson) => {
+            // return lesson.classIds.includes(classId) && lesson.dayIds.includes(1);
+            return lesson.groupIds.includes(groupId[0].id) && lesson.dayIds.includes(1);
+          })
+          .sort((a, b) => {
+            const hourIndexA = a.dayIds.indexOf(1);
+            const hourIndexB = b.dayIds.indexOf(1);
+
+            return a.hourIds[hourIndexA] - b.hourIds[hourIndexB];
+          })
+          .flatMap((data) => {
+            let names = [];
+            for (let i = 0; i < data.duration; i++) {
+              names[i] = schedule.subjects.find((subject) => subject.id == data.subjectId).name;
+            }
+            return names;
+          })
+      );
+    }
+  }, [selectedClass]);
+
   // przykład wyciągania danych
   //schedule.lessons.filter((lesson) => lesson.classIds == schedule.classes.find((klasa) => klasa.name == "4TP").id);
-  let lessonNames: string[] = [];
+  let classes: { label: string; value: number }[] = [];
   if (schedule != undefined) {
-    const classId = schedule.classes.find((klasa) => klasa.name == "2PS").id;
-    const groupId = schedule.groups.filter((grupa) => grupa.classId == classId);
-    const zgrupa = groupId.find(grupa => grupa.id == 261); // grupa zawodowa
-    const jgrupa = groupId.find(grupa => grupa.id == 253); // grupa językowa 
-    // cała klasa lesson.groupIds.includes(groupId[0].id)
-    // console.log(groupId); // aby wyświetlić wszystkie grupy, zmień klase u góry aby pokazać jej grupy
-    lessonNames = schedule.lessons
-      .filter((lesson) => {
-        // return lesson.classIds.includes(classId) && lesson.dayIds.includes(1);
-        return (lesson.groupIds.includes(zgrupa.id) || lesson.groupIds.includes(groupId[0].id) || lesson.groupIds.includes(jgrupa.id)) && lesson.dayIds.includes(1);
-      })
-      .sort((a, b) => {
-        const hourIndexA = a.dayIds.indexOf(1);
-        const hourIndexB = b.dayIds.indexOf(1);
+    //   const classId = schedule.classes.find((klasa) => klasa.name == "2PS").id;
+    //   const groupId = schedule.groups.filter((grupa) => grupa.classId == classId);
+    //   const zgrupa = groupId.find((grupa) => grupa.id == 261); // grupa zawodowa
+    //   const jgrupa = groupId.find((grupa) => grupa.id == 253); // grupa językowa
+    //   // cała klasa lesson.groupIds.includes(groupId[0].id)
+    //   // console.log(groupId); // aby wyświetlić wszystkie grupy, zmień klase u góry aby pokazać jej grupy
+    //   lessonNames = schedule.lessons
+    //     .filter((lesson) => {
+    //       // return lesson.classIds.includes(classId) && lesson.dayIds.includes(1);
+    //       return (
+    //         (lesson.groupIds.includes(zgrupa.id) ||
+    //           lesson.groupIds.includes(groupId[0].id) ||
+    //           lesson.groupIds.includes(jgrupa.id)) &&
+    //         lesson.dayIds.includes(1)
+    //       );
+    //     })
+    //     .sort((a, b) => {
+    //       const hourIndexA = a.dayIds.indexOf(1);
+    //       const hourIndexB = b.dayIds.indexOf(1);
 
-        return a.hourIds[hourIndexA] - b.hourIds[hourIndexB];
-      })
-      .flatMap((data) => {
-        let names = [];
-        for (let i = 0; i < data.duration; i++) {
-          names[i] = schedule.subjects.find((subject) => subject.id == data.subjectId).name;
-        }
-        return names;
-      });
+    //       return a.hourIds[hourIndexA] - b.hourIds[hourIndexB];
+    //     })
+    //     .flatMap((data) => {
+    //       let names = [];
+    //       for (let i = 0; i < data.duration; i++) {
+    //         names[i] = schedule.subjects.find((subject) => subject.id == data.subjectId).name;
+    //       }
+    //       return names;
+    //     });
+
+    classes = schedule.classes.map((klasa) => {
+      return { label: klasa.name, value: klasa.id };
+    });
   }
 
   return (
@@ -55,7 +93,7 @@ export default function App() {
           borderBottomRightRadius: 16
         }}
       >
-        <DropdownComponent></DropdownComponent>
+        <DropdownComponent data={classes} setExternalValue={setSelectedClass}></DropdownComponent>
       </View>
       {schedule == undefined && <Text style={{ alignSelf: "center", padding: "20%", fontSize: 36 }}>Ładowanie...</Text>}
       {schedule != undefined && (
--- src/components/dropdown.tsx
+++ src/components/dropdown.tsx
@@ -1,69 +1,63 @@
-import React, { useState } from 'react';
-import { StyleSheet } from 'react-native';
-import { Dropdown } from 'react-native-element-dropdown';
+import React, { useState } from "react";
+import { StyleSheet } from "react-native";
+import { Dropdown } from "react-native-element-dropdown";
 
+interface DropdownProps {
+  data: { label: string; value: number }[];
+  setExternalValue: (value: number) => void;
+}
 
-  const data = [
-    { label: 'Item 1', value: '1' },
-    { label: 'Item 2', value: '2' },
-    { label: 'Item 3', value: '3' },
-    { label: 'Item 4', value: '4' },
-    { label: 'Item 5', value: '5' },
-    { label: 'Item 6', value: '6' },
-    { label: 'Item 7', value: '7' },
-    { label: 'Item 8', value: '8' },
-  ];
+const DropdownComponent = (props: DropdownProps) => {
+  const [value, setValue] = useState(null);
 
-  const DropdownComponent = () => {
-    const [value, setValue] = useState(null);
+  return (
+    <Dropdown
+      style={styles.dropdown}
+      placeholderStyle={styles.placeholderStyle}
+      selectedTextStyle={styles.selectedTextStyle}
+      inputSearchStyle={styles.inputSearchStyle}
+      iconStyle={styles.iconStyle}
+      data={props.data}
+      search
+      maxHeight={300}
+      labelField="label"
+      valueField="value"
+      placeholder="Select item"
+      searchPlaceholder="Search..."
+      value={value}
+      onChange={(item) => {
+        setValue(item.value);
+        props.setExternalValue(item.value);
+      }}
+    />
+  );
+};
 
-    return (
-      <Dropdown
-        style={styles.dropdown}
-        placeholderStyle={styles.placeholderStyle}
-        selectedTextStyle={styles.selectedTextStyle}
-        inputSearchStyle={styles.inputSearchStyle}
-        iconStyle={styles.iconStyle}
-        data={data}
-        search
-        maxHeight={300}
-        labelField="label"
-        valueField="value"
-        placeholder="Select item"
-        searchPlaceholder="Search..."
-        value={value}
-        onChange={item => {
-          setValue(item.value);
-        }}
-      />
-    );
-  };
+export default DropdownComponent;
 
-  export default DropdownComponent;
-
-  const styles = StyleSheet.create({
-    dropdown: {
-      margin: 16,
-      height: 50,
-      borderBottomColor: 'gray',
-      borderBottomWidth: 0.5,
-    },
-    icon: {
-      marginRight: 5,
-    },
-    placeholderStyle: {
-      fontSize: 16,
-    },
-    selectedTextStyle: {
-      fontSize: 16,
-      color: "white",
-    },
-    iconStyle: {
-      width: 20,
-      height: 20,
-    },
-    inputSearchStyle: {
-      height: 40,
-      fontSize: 16,
-    },
-  });
+const styles = StyleSheet.create({
+  dropdown: {
+    margin: 16,
+    height: 50,
+    borderBottomColor: "gray",
+    borderBottomWidth: 0.5
+  },
+  icon: {
+    marginRight: 5
+  },
+  placeholderStyle: {
+    fontSize: 16
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+    color: "white"
+  },
+  iconStyle: {
+    width: 20,
+    height: 20
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16
+  }
+});
