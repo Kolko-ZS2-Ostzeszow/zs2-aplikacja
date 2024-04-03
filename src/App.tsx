2dcf3081353c8cef9d81a0c8b5445639b7d8b24c
SharpMars <70289680+SharpMars@users.noreply.github.com>
2024-04-03 21:14:41 +0200

    Group selection

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,40 @@ import Lesson from "./components/lesson";
 import { Accent1 } from "./theme";
 import { Dropdown } from "react-native-element-dropdown";
 import DropdownComponent from "./components/dropdown";
+import MultiDropdownComponent from "./components/multi_dropdown";
 
 export default function App() {
   let [schedule, setSchedule] = useState<ScheduleData>();
   let [selectedClass, setSelectedClass] = useState<number>(null);
   let [lessonNames, setLessonNames] = useState<string[]>([]);
+  let [classGroups, setClassGroups] = useState<{ label: string; value: number }[]>([]);
+  let [selectedGroups, setSelectedGroups] = useState<number[]>([]);
 
   useEffect(() => {
     fetchEdupageData().then((data) => setSchedule(data));
   }, []);
 
   useEffect(() => {
+    if (schedule == undefined) return;
+
+    setClassGroups(
+      schedule.groups
+        .filter((grupa) => grupa.classId == selectedClass && !grupa.entireClass)
+        .map((grupa) => {
+          return { label: grupa.name, value: grupa.id };
+        })
+    );
+  }, [selectedClass]);
+
+  useEffect(() => {
     if (schedule != undefined) {
       const classId = schedule.classes.find((klasa) => klasa.id == selectedClass).id;
-      const groupId = schedule.groups.filter((grupa) => grupa.classId == classId);
+      const groupId = schedule.groups.find((grupa) => grupa.classId == classId && grupa.entireClass).id;
       setLessonNames(
         schedule.lessons
           .filter((lesson) => {
             // return lesson.classIds.includes(classId) && lesson.dayIds.includes(1);
-            return lesson.groupIds.includes(groupId[0].id) && lesson.dayIds.includes(1);
+            return lesson.groupIds.some((id) => [...selectedGroups, groupId].includes(id)) && lesson.dayIds.includes(1);
           })
           .sort((a, b) => {
             const hourIndexA = a.dayIds.indexOf(1);
@@ -41,7 +56,7 @@ export default function App() {
           })
       );
     }
-  }, [selectedClass]);
+  }, [selectedClass, selectedGroups]);
 
   // przykład wyciągania danych
   //schedule.lessons.filter((lesson) => lesson.classIds == schedule.classes.find((klasa) => klasa.name == "4TP").id);
@@ -80,6 +95,9 @@ export default function App() {
     classes = schedule.classes.map((klasa) => {
       return { label: klasa.name, value: klasa.id };
     });
+
+    if (classes.length > 0 && selectedClass == null) {
+    }
   }
 
   return (
@@ -88,12 +106,13 @@ export default function App() {
         style={{
           paddingTop: Platform.OS === "android" ? 25 : 0,
           backgroundColor: Accent1,
-          height: "10%",
+          height: "20%",
           borderBottomLeftRadius: 16,
           borderBottomRightRadius: 16
         }}
       >
         <DropdownComponent data={classes} setExternalValue={setSelectedClass}></DropdownComponent>
+        <MultiDropdownComponent data={classGroups} setExternalValue={setSelectedGroups}></MultiDropdownComponent>
       </View>
       {schedule == undefined && <Text style={{ alignSelf: "center", padding: "20%", fontSize: 36 }}>Ładowanie...</Text>}
       {schedule != undefined && (
---
+++ src/components/multi_dropdown.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { StyleSheet } from "react-native";
+import { Dropdown, MultiSelect } from "react-native-element-dropdown";
+
+interface MultiDropdownProps {
+  data: { label: string; value: number }[];
+  setExternalValue: (value: number[]) => void;
+}
+
+const MultiDropdownComponent = (props: MultiDropdownProps) => {
+  const [value, setValue] = useState(null);
+
+  return (
+    <MultiSelect
+      style={styles.dropdown}
+      placeholderStyle={styles.placeholderStyle}
+      selectedTextStyle={styles.selectedTextStyle}
+      inputSearchStyle={styles.inputSearchStyle}
+      iconStyle={styles.iconStyle}
+      data={props.data}
+      search
+      maxHeight={300}
+      labelField="label"
+      valueField="value"
+      placeholder="Select item"
+      searchPlaceholder="Search..."
+      value={value}
+      onChange={(item) => {
+        setValue(item);
+        props.setExternalValue(item.map((i) => parseInt(i)));
+      }}
+    />
+  );
+};
+
+export default MultiDropdownComponent;
+
+const styles = StyleSheet.create({
+  dropdown: {
+    margin: 16,
+    height: 50,
+    borderBottomColor: "gray",
+    borderBottomWidth: 0.5
+  },
+  icon: {
+    marginRight: 5
+  },
+  placeholderStyle: {
+    fontSize: 16
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+    color: "white"
+  },
+  iconStyle: {
+    width: 20,
+    height: 20
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16
+  }
+});
