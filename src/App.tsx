c9613230b91b411693a796521ca71e1b01bee916
SharpMars <70289680+SharpMars@users.noreply.github.com>
2024-04-25 21:43:34 +0200

    Remove dead code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, FlatList, Platform, RefreshControl, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { Button, FlatList, Platform, RefreshControl, StyleSheet, Text, View } from "react-native";
 import { Days, fetchEdupageData } from "./utils/edupage";
 import { useEffect, useState } from "react";
 import Lesson from "./components/lesson";
 import { Accent1 } from "./theme";
-import { Dropdown } from "react-native-element-dropdown";
 import DropdownComponent from "./components/dropdown";
 import MultiDropdownComponent from "./components/multi_dropdown";
 import React from "react";
@@ -93,40 +92,8 @@ function App() {
     }
   }, [selectedClass, selectedGroups, dayId]);
 
-  // przykład wyciągania danych
-  //schedule.lessons.filter((lesson) => lesson.classIds == schedule.classes.find((klasa) => klasa.name == "4TP").id);
   let classes: { label: string; value: number }[] = [];
   if (scheduleQuery.data != undefined) {
-    //   const classId = schedule.classes.find((klasa) => klasa.name == "2PS").id;
-    //   const groupId = schedule.groups.filter((grupa) => grupa.classId == classId);
-    //   const zgrupa = groupId.find((grupa) => grupa.id == 261); // grupa zawodowa
-    //   const jgrupa = groupId.find((grupa) => grupa.id == 253); // grupa językowa
-    //   // cała klasa lesson.groupIds.includes(groupId[0].id)
-    //   // console.log(groupId); // aby wyświetlić wszystkie grupy, zmień klase u góry aby pokazać jej grupy
-    //   lessonNames = schedule.lessons
-    //     .filter((lesson) => {
-    //       // return lesson.classIds.includes(classId) && lesson.dayIds.includes(1);
-    //       return (
-    //         (lesson.groupIds.includes(zgrupa.id) ||
-    //           lesson.groupIds.includes(groupId[0].id) ||
-    //           lesson.groupIds.includes(jgrupa.id)) &&
-    //         lesson.dayIds.includes(1)
-    //       );
-    //     })
-    //     .sort((a, b) => {
-    //       const hourIndexA = a.dayIds.indexOf(1);
-    //       const hourIndexB = b.dayIds.indexOf(1);
-
-    //       return a.hourIds[hourIndexA] - b.hourIds[hourIndexB];
-    //     })
-    //     .flatMap((data) => {
-    //       let names = [];
-    //       for (let i = 0; i < data.duration; i++) {
-    //         names[i] = schedule.subjects.find((subject) => subject.id == data.subjectId).name;
-    //       }
-    //       return names;
-    //     });
-
     classes = scheduleQuery.data.classes.map((klasa) => {
       return { label: klasa.name, value: klasa.id };
     });
