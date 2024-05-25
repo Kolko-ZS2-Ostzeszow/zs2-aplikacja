4ca091597f880f2108f8f9f1cbe25bf7794a3c22
SharpMars <70289680+SharpMars@users.noreply.github.com>
2024-05-25 13:21:49 +0200

    Change schedule tab icon to 'calendar'

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -8,7 +8,7 @@ export default function TabLayout() {
         name="index"
         options={{
           title: "Plan",
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />
+          tabBarIcon: ({ color }) => <FontAwesome size={24} name="calendar" color={color} />
         }}
       />
     </Tabs>
